99f0e105ea54f5407ae3d1eb8ba34fccae9ea644
Vaibhav Kolekar <68164031+vaibhav-kolekar@users.noreply.github.com>
2026-07-16 22:41:06 +0530

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.java
@@ -0,0 +1,24 @@
+class MyHashMap {
+
+    int arr[];
+
+    public MyHashMap() {
+        arr = new int[1000001];
+        for(int x =0; x < arr.length; x ++){
+            arr[x] = -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    public int get(int key) {
+        return arr[key];
+    }
+    
+    public void remove(int key) {
+        arr[key] = -1;
+    }
+}
+
